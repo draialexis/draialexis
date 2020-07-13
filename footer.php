c52738bd5dbe6edbdf79c8ff84f4d8c86c00dadc
Alexis DRAI <drai.alexis@gmail.com>
2020-07-13 22:45:18 +0200

    minor footer update

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <footer id="footer">
-  	<p><span id="copyright">&copy;ribBit 2019</span></p>
+  	<p><span id="copyright">&copy;ribBit 2019 - 2020</span></p>
   	<button class="btn btn-footer"><a href="https://github.com/Rib-bit" target="_blank"><i class="fa fa-github" style="color: #fff6e7" aria-hidden="true"></i></a></button>
   	<button class="btn btn-footer"><a href="https://www.freecodecamp.org/ribbit" target="_blank"><i class="fa fa-free-code-camp" style="color: #fff6e7" aria-hidden="true"></i></a></button>
   	<button class="btn btn-footer"><a href="https://www.linkedin.com/in/alexisdrai/" target="_blank"><i class="fa fa-linkedin" style="color: #fff6e7" aria-hidden="true"></i></a></button>
