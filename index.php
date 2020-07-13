86947dcfd44baadc3970fffdc098ef6129ab62c7
Alexis DRAI <drai.alexis@gmail.com>
2020-07-13 17:38:29 +0200

    draialexis git

---
+++ index.php
@@ -0,0 +1,102 @@
+<?php session_start();?>
+<!DOCTYPE html>
+<html lang="fr" itemscope itemtype="http://schema.org/WebSite">
+<head>
+	<meta charset="utf-8">
+	<!--Bootstrap stylesheet-->
+	<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<!--FontAwesome stylesheet-->
+	<link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+	<!--Google Font stylesheets-->
+
+	<meta name="description" content="Developpeur Java - Alexis DRAI. Ceci est ma vitrine sur le web. Elle est destinée à devenir mon cv, le répertoire central de mes accomplissements, 
+	et une façon pratique pour les usagers d'en apprendre plus à mon sujet ; et de me contacter." />
+	<!-- Schema.org markup for Google+ -->
+	<meta itemprop="name" content="Developpeur Java - Alexis DRAI">
+	<meta itemprop="description" content="Ceci est ma vitrine sur le web. Elle est destinée à devenir mon cv, le répertoire central de mes accomplissements, 
+	et une façon pratique pour les usagers d'en apprendre plus à mon sujet ; et de me contacter.">
+	<meta itemprop="image" content="http://www.alexisdrai.com/img/developpeur-java-alexis-drai.jpg">
+	<!-- Twitter Card data -->
+	<meta name="twitter:card" content="summary_large_image">
+	<meta name="twitter:site" content="@rybByt_">
+	<meta name="twitter:title" content="Developpeur Java - Alexis DRAI">
+	<meta name="twitter:description" content="Ceci est ma vitrine sur le web. Elle est destinée à devenir mon cv, le répertoire central de mes accomplissements, 
+	et une façon pratique pour les usagers d'en apprendre plus à mon sujet ; et de me contacter.">
+	<meta name="twitter:creator" content="@rybByt_">
+	<!-- Twitter summary card with large image must be at least 280x150px -->
+	<meta name="twitter:image:src" content="http://www.alexisdrai.com/img/developpeur-java-alexis-drai.jpg">
+	<!-- Open Graph data -->
+	<meta property="og:title" content="Developpeur Java - Alexis DRAI" />
+	<meta property="og:type" content="webpage" />
+	<meta property="og:url" content="http://www.alexisdrai.com" />
+	<meta property="og:image" content="http://www.alexisdrai.com/img/developpeur-java-alexis-drai.jpg" />
+	<meta property="og:description" content="Ceci est ma vitrine sur le web. Elle est destinée à devenir mon cv, le répertoire central de mes accomplissements, 
+	et une façon pratique pour les usagers d'en apprendre plus à mon sujet ; et de me contacter." />
+	<meta property="og:site_name" content="Developpeur Java - Alexis DRAI" />
+	<meta property="article:published_time" content="2019-11-01T00:00:00+01:00" />
+	<meta property="article:modified_time" content="2019-11-07T00:00:00+01:00" />
+	
+	<link href="https://fonts.googleapis.com/css?family=Asap&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Lato&display=swap" rel="stylesheet">
+	<!--CSS stylesheet-->
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>Developpeur Java - Alexis DRAI</title>
+</head>
+
+<body>
+
+	<?php include "sidebar.php";?>
+
+	<div id="main-wrapper">
+
+		<?php include "header.php";?>
+
+		<div id="section-wrapper">
+			<section>
+				<h2>Mon expérience</h2>
+				<p>
+					<span class="date">10-2019 à 02-2020</span> : <strong>Développeur Java Fullstack</strong> @ <a class="link" href="https://onelogic.fr" target="_blank">One Logic Consulting Group</a>, Boulogne-Billancourt<br /><br />
+					<span class="date">07-2018 à 09-2019</span> : <strong>Codeur</strong> participant aux challenges <a class="link" href="https://www.100daysofcode.com" target="_blank">#100DaysOfCode</a> et <a class="link" href="https://301daysofcode.com" target="_blank">#301DaysOfCode</a>, spécialité java<br />
+					<span class="date">06-2016 à 06-2018</span> : <strong>Chef de rang</strong> @ <a class="link" href="https://www.angelina-paris.fr" target="_blank">Angelina</a> - Salon de thé, Versailles<br />
+					<span class="date">11-2014 à 05-2016</span> : <strong>Commis de salle</strong> @ <a class="link" href="https://www.angelina-paris.fr" target="_blank">Angelina</a> - Salon de thé, Versailles<br />
+					<span class="date">07-2013 à 07-2015</span> : <strong>Professeur particulier d'anglais</strong> @ Freelance, Yvelines & Paris Ouest<br />
+					<span class="date">09-2012 à 06-2013</span> : <strong>Assistant de langues vivantes</strong> @ <a class="link" href="https://www.dollaracademy.org.uk" target="_blank">Dollar Academy</a>, Dollar, Ecosse, Royaume-Uni
+				</p>
+			</section>
+			<section>
+				<h2>Ma formation</h2>
+				<p>
+					<span class="date">depuis 06-2019</span> : <strong>Parcours Développeur d'application Java</strong> RNCP niveau II / Bac+4 - <a class="link" href="https://openclassrooms.com" target="_blank">Openclassrooms</a><br /><br />
+					<span class="date">depuis 06-2018</span> : <strong>Certifications en ligne</strong> - <a class="link" href="https://openclassrooms.com" target="_blank">OpenClassrooms</a>, <a class="link" href="https://www.udemy.com" target="_blank">Udemy</a> et <a class="link" href="https://www.freecodecamp.org" target="_blank">freeCodeCamp</a><br />
+					<span class="date">09-2009 à 06-2012</span> : <strong>Langues, Littératures, et Civilisations Etrangères, mention : anglais</strong> Licence / Bac+3 - <a class="link" href="http://www.uvsq.fr" target="_blank">Université UVSQ</a>, Guyancourt<br />
+					<span class="date">12-2011 à 06-2012</span> : <strong>S6 à l'étranger</strong> -  <a class="link" href="https://www.westminster.ac.uk" target="_blank">University of Westminster</a>, Londres, Angleterre, Royaume-Uni
+				</p>
+			</section>
+			<section>
+
+				<h2>Mes compétences</h2>
+				<p>
+					<strong>Backend</strong> - Java, JavaScript, TypeScript, JEE, Spring, SQL<br />
+					<strong>Frontend</strong> - HTML, CSS, XML, Angular, Bootstrap, jQuery<br />
+					<strong>Misc.</strong>	- Git, GitHub, Swagger, IntelliJ, XAMPP, UML, MVC, OOP, FP<br />
+				</p>
+			</section>
+		</div>
+	</div>
+
+	<?php include "footer.php";?>
+<script type='application/ld+json'>
+{
+  "@context": "http://www.schema.org",
+  "@type": "person",
+  "name": "Alexis Drai",
+  "jobTitle": "Java Developer",
+  "url": "http://www.alexisdrai.com",
+  "email": "info@alexisdrai.com",
+  "telephone": "+33663620277",
+  "birthDate": "1990-04-29"
+}
+ </script>
+</body>
+
+</html>
