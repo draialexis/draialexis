dea809105e3744f660b8b624090847836304ebc0
Alexis DRAI <drai.alexis@gmail.com>
2020-09-18 18:54:56 +0200

    Clean up

--- footer.php
+++
@@ -1,7 +0,0 @@
-<footer id="footer">
-  	<p><span id="copyright">&copy;ribBit 2019 - 2020</span></p>
-  	<button class="btn btn-footer"><a href="https://github.com/Rib-bit" target="_blank"><i class="fa fa-github" style="color: #fff6e7" aria-hidden="true"></i></a></button>
-  	<button class="btn btn-footer"><a href="https://www.freecodecamp.org/ribbit" target="_blank"><i class="fa fa-free-code-camp" style="color: #fff6e7" aria-hidden="true"></i></a></button>
-  	<button class="btn btn-footer"><a href="https://www.linkedin.com/in/alexisdrai/" target="_blank"><i class="fa fa-linkedin" style="color: #fff6e7" aria-hidden="true"></i></a></button>
-    <button class="btn btn-footer"><a href="https://twitter.com/rybByt_" target="_blank"><i class="fa fa-twitter" style="color: #fff6e7" aria-hidden="true"></i></a></button>
-</footer>
