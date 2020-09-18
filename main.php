a5be5dd303e09bf62f32c56c27ee3788626f2070
Alexis DRAI <drai.alexis@gmail.com>
2020-09-18 17:56:01 +0200

    Move main section into own php component

---
+++ main.php
@@ -0,0 +1,58 @@
+<div id="section-wrapper">
+    <section>
+        <h2>Mon expérience</h2>
+        <p>
+            <span class="date">10-2019 à 02-2020</span> : <strong>Développeur Java Fullstack</strong> @ <a
+                    class="link" href="https://onelogic.fr" target="_blank">One Logic Consulting Group</a>,
+            Boulogne-Billancourt<br/><br/>
+            <span class="date">07-2018 à 09-2019</span> : <strong>Codeur</strong> participant aux challenges <a
+                    class="link" href="https://www.100daysofcode.com" target="_blank">#100DaysOfCode</a> et <a
+                    class="link" href="https://301daysofcode.com" target="_blank">#301DaysOfCode</a>, spécialité
+            java<br/>
+            <span class="date">06-2016 à 06-2018</span> : <strong>Chef de rang</strong> @ <a class="link"
+                                                                                             href="https://www.angelina-paris.fr"
+                                                                                             target="_blank">Angelina</a>
+            - Salon de thé, Versailles<br/>
+            <span class="date">11-2014 à 05-2016</span> : <strong>Commis de salle</strong> @ <a class="link"
+                                                                                                href="https://www.angelina-paris.fr"
+                                                                                                target="_blank">Angelina</a>
+            - Salon de thé, Versailles<br/>
+            <span class="date">07-2013 à 07-2015</span> : <strong>Professeur particulier d'anglais</strong> @
+            Freelance, Yvelines & Paris Ouest<br/>
+            <span class="date">09-2012 à 06-2013</span> : <strong>Assistant de langues vivantes</strong> @ <a
+                    class="link" href="https://www.dollaracademy.org.uk" target="_blank">Dollar Academy</a>, Dollar,
+            Ecosse, Royaume-Uni
+        </p>
+    </section>
+    <section>
+        <h2>Ma formation</h2>
+        <p>
+            <span class="date">depuis 06-2019</span> : <strong>Parcours Développeur d'application Java</strong> RNCP
+            niveau II / Bac+4 - <a class="link" href="https://openclassrooms.com"
+                                   target="_blank">Openclassrooms</a><br/><br/>
+            <span class="date">depuis 06-2018</span> : <strong>Certifications en ligne</strong> - <a class="link"
+                                                                                                     href="https://openclassrooms.com"
+                                                                                                     target="_blank">OpenClassrooms</a>,
+            <a class="link" href="https://www.udemy.com" target="_blank">Udemy</a> et <a class="link"
+                                                                                         href="https://www.freecodecamp.org"
+                                                                                         target="_blank">freeCodeCamp</a><br/>
+            <span class="date">09-2009 à 06-2012</span> : <strong>Langues, Littératures, et Civilisations
+                Etrangères, mention : anglais</strong> Licence / Bac+3 - <a class="link" href="http://www.uvsq.fr"
+                                                                            target="_blank">Université UVSQ</a>,
+            Guyancourt<br/>
+            <span class="date">12-2011 à 06-2012</span> : <strong>S6 à l'étranger</strong> - <a class="link"
+                                                                                                href="https://www.westminster.ac.uk"
+                                                                                                target="_blank">University
+                of Westminster</a>, Londres, Angleterre, Royaume-Uni
+        </p>
+    </section>
+    <section>
+
+        <h2>Mes compétences</h2>
+        <p>
+            <strong>Backend</strong> - Java, JavaScript, TypeScript, JEE, Spring, SQL<br/>
+            <strong>Frontend</strong> - HTML, CSS, XML, Angular, Bootstrap, jQuery<br/>
+            <strong>Misc.</strong> - Git, GitHub, Swagger, IntelliJ, XAMPP, UML, MVC, OOP, FP<br/>
+        </p>
+    </section>
+</div>
