9a1934f49fe3e7a955171901b1807f284fe85b26
Alexis DRAI <drai.alexis@gmail.com>
2020-09-18 19:43:19 +0200

    Reword 'dev'

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 <header>
-	<img src="img/developpeur-java-alexis-drai.jpg" alt="">
+	<img src="img/dev-java-alexis-drai.jpg" alt="">
 	<div>
 		<div><h1>Alexis DRAI</h1></div>
 		<div><p><br /><strong>Développeur Java</strong> junior, polyglotte studieux et méthodique, ancien de la restauration, j'aime la logique et
--- index.php
+++ index.php
@@ -11,30 +11,30 @@
           integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
     <!--Google Font stylesheets-->
 
-    <meta name="description" content="Developpeur Java - Alexis DRAI. Ceci est ma vitrine sur le web. Elle est destinée à devenir mon cv, le répertoire central de mes accomplissements,
+    <meta name="description" content="Dev Java - Alexis DRAI. Ceci est ma vitrine sur le web. Elle est destinée à devenir mon cv, le répertoire central de mes accomplissements,
 	et une façon pratique pour les usagers d'en apprendre plus à mon sujet ; et de me contacter."/>
     <!-- Schema.org markup for Google+ -->
-    <meta itemprop="name" content="Developpeur Java - Alexis DRAI">
+    <meta itemprop="name" content="Dev Java - Alexis DRAI">
     <meta itemprop="description" content="Ceci est ma vitrine sur le web. Elle est destinée à devenir mon cv, le répertoire central de mes accomplissements,
 	et une façon pratique pour les usagers d'en apprendre plus à mon sujet ; et de me contacter.">
-    <meta itemprop="image" content="http://www.alexisdrai.com/img/developpeur-java-alexis-drai.jpg">
+    <meta itemprop="image" content="http://www.alexisdrai.com/img/dev-java-alexis-drai.jpg">
     <!-- Twitter Card data -->
     <meta name="twitter:card" content="summary_large_image">
     <meta name="twitter:site" content="@rybByt_">
-    <meta name="twitter:title" content="Developpeur Java - Alexis DRAI">
+    <meta name="twitter:title" content="Dev Java - Alexis DRAI">
     <meta name="twitter:description" content="Ceci est ma vitrine sur le web. Elle est destinée à devenir mon cv, le répertoire central de mes accomplissements,
 	et une façon pratique pour les usagers d'en apprendre plus à mon sujet ; et de me contacter.">
     <meta name="twitter:creator" content="@rybByt_">
     <!-- Twitter summary card with large image must be at least 280x150px -->
-    <meta name="twitter:image:src" content="http://www.alexisdrai.com/img/developpeur-java-alexis-drai.jpg">
+    <meta name="twitter:image:src" content="http://www.alexisdrai.com/img/dev-java-alexis-drai.jpg">
     <!-- Open Graph data -->
-    <meta property="og:title" content="Developpeur Java - Alexis DRAI"/>
+    <meta property="og:title" content="Dev Java - Alexis DRAI"/>
     <meta property="og:type" content="webpage"/>
     <meta property="og:url" content="http://www.alexisdrai.com"/>
-    <meta property="og:image" content="http://www.alexisdrai.com/img/developpeur-java-alexis-drai.jpg"/>
+    <meta property="og:image" content="http://www.alexisdrai.com/img/dev-java-alexis-drai.jpg"/>
     <meta property="og:description" content="Ceci est ma vitrine sur le web. Elle est destinée à devenir mon cv, le répertoire central de mes accomplissements,
 	et une façon pratique pour les usagers d'en apprendre plus à mon sujet ; et de me contacter."/>
-    <meta property="og:site_name" content="Developpeur Java - Alexis DRAI"/>
+    <meta property="og:site_name" content="Dev Java - Alexis DRAI"/>
     <meta property="article:published_time" content="2019-11-01T00:00:00+01:00"/>
     <meta property="article:modified_time" content="2019-11-07T00:00:00+01:00"/>
 
@@ -43,7 +43,7 @@
     <!--CSS stylesheet-->
     <link rel="stylesheet" type="text/css" href="style.css">
     <link href="img/favicon.ico" rel="icon" type="image/x-icon">
-    <title>Developpeur Java - Alexis DRAI</title>
+    <title>Dev Java - Alexis DRAI</title>
 </head>
 
 <body>
